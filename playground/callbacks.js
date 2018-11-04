eaae4bf15f915eb18e9a9066064f5e8c503fa717
Jeff <none>
2018-11-04 20:33:16 +0000

    What's makesp up an HTTP request

--- playground/callbacks.js
+++ playground/callbacks.js
@@ -12,11 +12,3 @@ var getUser = (id, callback) => {
 getUser(31, (user) => {
 	console.log(user);
 });
-
-/*
-http://www.mapquestapi.com/geocoding/v1/address?key=jAr8wJxTbwFSTsm6qVA7HS13Cda8DUTi&location=1301%20lombard%20street%20philadelphia
-
-The latitude is stored on the response body here: body.results[0].locations[0].latLng.lat
-
-The longitude is stored on the response body here: body.results[0].locations[0].latLng.lng
-*/
