64123eb0e840a92da418a096492db8f2c3ba2c4c
Jeff <none>
2018-11-04 21:03:01 +0000

    Encoding user input

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+	.options({
+		a: {
+			demand: true,
+			alias: 'address',
+			describe: 'Address to fetch weather for',
+			string: true
+		}
+	})
+	.help()
+	.alias('help','h')
+	.argv;
+
+var encodedAddress = encodeURIComponent(argv.address);
 
 request({
-	url: 'http://www.mapquestapi.com/geocoding/v1/address?key=jAr8wJxTbwFSTsm6qVA7HS13Cda8DUTi&location=1301%20lombard%20street%20philadelphia',
+	url: `http://www.mapquestapi.com/geocoding/v1/address?key=jAr8wJxTbwFSTsm6qVA7HS13Cda8DUTi&location=${encodedAddress}`,
 	json: true
 }, (error, response, body) => {
 	console.log(`Address ${body.results[0].providedLocation.location}`);
